a9d962dac689b14f8cac04e8fa865769d632c2ab
Wilhelmsen <wwilhelmsen@sogn.no>
2025-02-15 13:40:47 +0100

    feat: Added a simple example menu bar
    Updates: #9

--- source/window.py
+++ source/window.py
@@ -1,12 +1,27 @@
 #!/usr/bin/env python3
 import sys
+from PyQt6.QtGui import (
+    QAction,
+    QKeySequence,
+)
 from PyQt6.QtWidgets import (
-    QApplication, QWidget, QLineEdit, QPushButton, QHBoxLayout, QMainWindow,
+    QApplication,
+    QHBoxLayout,
+    QLineEdit,
+    QMainWindow,
+    QMenu,
+    QMenuBar,
+    QPushButton,
+    QWidget,
 )
 
 
 Const = {
-    "WINDOW_TITLE" : "Latent Space Visualizer",
+    "WINDOW_TITLE": "Latent Space Visualizer",
+    "FILE_FILTERS": [
+        "PyTorch Files (*.pt)",
+        "All Files (*.*)",
+    ],
 }
 
 
@@ -14,11 +29,32 @@ class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.setWindowTitle(const["WINDOW_TITLE"])
+        self.initiate_menu_bar()
+
+        self.setWindowTitle(Const["WINDOW_TITLE"])
 
         button = QPushButton("Button")
         self.setCentralWidget(button)
 
+
+    def initiate_menu_bar(self):
+        menu = self.menuBar()
+        action_to_open_file = QAction("&Open File", self)
+        action_to_open_file.setStatusTip("TODO: Make real status tip")
+        action_to_open_file.setCheckable(True)
+        action_to_open_file.setShortcut(QKeySequence("Ctrl+O"))
+
+
+        bogus_menu = menu.addMenu("&Bogus")
+        bogus_menu.addAction(action_to_open_file)
+        bogus_menu.addAction(QAction("Scrogus", self))
+
+
+    def get_filename(self):
+        initial_filter = FILE_FILTERS[3] # Select one from the list.
+        filters = ";;".join(FILE_FILTERS)
+        print("Filters are:", filters)
+        print("Initial filter:", initial_filter)
 
 
 class SomeWindow(QWidget):
