f04b56d7d607ccad69d83beb5bf10f4737900634
Linnea <linnea.tjore@gmail.com>
2025-02-26 11:47:08 -0500

    Rename: variables in open_file_dialogue

--- source/main_window.py
+++ source/main_window.py
@@ -83,7 +83,7 @@ class MainWindow(QMainWindow):
         action_to_open_file = QAction(consts.OPEN_FILE_LABEL, self)
         action_to_open_file.setStatusTip(consts.STATUS_TIP_TEMP)
         action_to_open_file.setShortcut(QKeySequence("Ctrl+O"))
-        action_to_open_file.triggered.connect(self.open_file_dialog)
+        action_to_open_file.triggered.connect(self.open_file_dialogue)
         # Note --->function Raference         ^^^^^^^^^^^^^^^^^^^^^
 
         next_tab = QAction("TEMP: &Next tab")
@@ -107,17 +107,17 @@ class MainWindow(QMainWindow):
         self.prev_tab = prev_tab
         self.file_menu = file_menu
 
-    def open_file_dialog(self):
+    def open_file_dialogue(self):
         initial_filter = consts.FILE_FILTERS[0]
         filters = ";;".join(consts.FILE_FILTERS)
 
         # TODO: Consider QFileDialog: {FileMode, Acceptmode, "Options"}
-        filename, selected_filter = QFileDialog.getOpenFileName(
+        filepath, selected_filter = QFileDialog.getOpenFileName(
             self,
             filter=filters,
             initialFilter=initial_filter,
         )
-        print("Result:", filename, selected_filter)
+        print("Result:", filepath, selected_filter)
 
     def activate_tab_0(self):
         self.tab_layout.setCurrentIndex(0)
