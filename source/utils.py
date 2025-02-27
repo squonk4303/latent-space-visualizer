1595badccb60814ba83c3964d5881d882fa27594
Linnea <linnea.tjore@gmail.com>
2025-02-27 16:45:21 -0500

    Simplified arr_is_subset

--- source/utils.py
+++ source/utils.py
@@ -2,6 +2,4 @@ def arr_is_subset(arr1, arr2):
     """
     Compare if arr1 is part of arr2, return bool
     """
-    a = arr1
-    b = arr2
-    return set(a) <= set(b)
+    return set(arr1) <= set(arr2)
