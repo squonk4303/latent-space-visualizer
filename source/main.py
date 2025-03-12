b7232a19c8173f0162be8cc5d0714489fe63255b
Wilhelmsen <wwilhelmsen@sogn.no>
2025-03-12 15:49:31 -0400

    Add argument for setting seed
    Updates: #36

--- source/main.py
+++ source/main.py
@@ -4,6 +4,7 @@ import sys
 from PyQt6.QtWidgets import QApplication
 from view_manager import PrimaryWindow
 import consts
+import utils
 
 if __name__ == "__main__":
     # Set up for command-line arguments
@@ -13,9 +14,17 @@ if __name__ == "__main__":
     parser.add_argument(
         "--xkcd", help="display the plot in a different style", action="store_true"
     )
+    parser.add_argument(
+        "-s", "--seed", type=int, help="set seed for all relevant packages"
+    )
 
     args = parser.parse_args()
+
+    # Handle arguments
     consts.flags["xkcd"] = args.xkcd
+
+    if args.seed is not None:
+        utils.superseed(int(args.seed))
 
     # Define the GUI and run it
     app = QApplication(sys.argv)
--- source/utils.py
+++ source/utils.py
@@ -1,5 +1,17 @@
+#!/usr/bin/env python3
+import random
+
+import numpy as np
+import torch
+
+
 def arr_is_subset(arr1, arr2):
-    """
-    Compare if arr1 is part of arr2, return bool
-    """
+    """Compare if arr1 is part of arr2, return bool."""
     return set(arr1) <= set(arr2)
+
+
+def superseed(seed):
+    """Set seed for python, numpy and torch."""
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
