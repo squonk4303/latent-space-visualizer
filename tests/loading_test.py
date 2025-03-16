246f76ac519f5f3102d168aa54a4390cd803b041
Wilhelmsen <wwilhelmsen@sogn.no>
2025-03-16 14:32:50 -0400

    Add some tests for the jpeg grabber function
    Updates: #40

--- tests/loading_test.py
+++ tests/loading_test.py
@@ -7,7 +7,7 @@ from PyQt6.QtWidgets import (
     QMainWindow,
 )
 
-from visualizer import loading
+from visualizer import loading, utils
 from visualizer import open_dialog
 
 
@@ -53,3 +53,75 @@ def test_model_path_set():
     with mocked_qfiledialog:
         path = open_dialog.for_trained_model_file(parent=None)
         assert path == "a/gorgeous/path"
+
+
+# TEMP: Evaluating test_fixtures
+import tempfile
+import os
+
+
+def test_1():
+    """
+    @Linnea: Question from W: does naming convention make sense?
+    Variable names for the test file structures are a bit like this:
+    (Though the names when running are random; determined by 'tempfile')
+
+    .
+    ├── dir_100
+    │   ├── file_101
+    │   ├── file_102
+    │   ├── file_103
+    │   └── dir_110
+    │       ├── file_111
+    │       ├── file_112
+    │       └── file_113
+    └── dir_200
+        ├── file_201
+        ├── file_202
+        └── file_203
+    """
+
+    dir_100 = tempfile.TemporaryDirectory()
+    file_101 = tempfile.NamedTemporaryFile(dir=dir_100.name, suffix=".jpeg")
+    file_102 = tempfile.NamedTemporaryFile(dir=dir_100.name, suffix=".jpeg")
+    file_103 = tempfile.NamedTemporaryFile(dir=dir_100.name, suffix=".jpeg")
+
+    dir_110 = tempfile.TemporaryDirectory(dir=dir_100.name)
+    file_111 = tempfile.NamedTemporaryFile(dir=dir_110.name)
+    file_112 = tempfile.NamedTemporaryFile(dir=dir_110.name)
+    file_113 = tempfile.NamedTemporaryFile(dir=dir_110.name)
+
+    dir_200 = tempfile.TemporaryDirectory()
+    file_201 = tempfile.NamedTemporaryFile(dir=dir_200.name)
+    file_202 = tempfile.NamedTemporaryFile(dir=dir_200.name)
+    file_203 = tempfile.NamedTemporaryFile(dir=dir_200.name)
+
+    # Write something to file
+    with open(file_101.name, "w") as f:
+        f.write("Yahaha! You found me!")
+
+    # Check contents of file
+    with open(file_101.name) as f:
+        content = f.read()
+        print(content)
+        assert content == "Yahaha! You found me!"
+
+    # Check ls of dir
+    filepaths = [os.path.join(dir_100.name, f) for f in os.listdir(dir_100.name)]
+    print("FILES:", filepaths)
+
+
+# Check that if the function runs in a populated directory, it gets all the image files
+def imagegrabber_gets_any_extension():
+    pass
+
+def imagegrabber_gets_jpgs():
+    pass
+# Check that it can handle dirs outside of the repo
+# ^^ What about dirs from protected areas? Raise exception on illegal dirs?
+# Check that if the function runs in a populated directory, it ignores the non-image files
+# Check that if the function runs in an unpopulated directory, it returns empty
+# Check that the function gets from current directory and NOT subdirectories (recursively)
+# Check that it doesn't follow symlinks (if it recurses)
+# Check that it doesn't follow looping symlinks
+# Check that it does with multiple filetypes at once
