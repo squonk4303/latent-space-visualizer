a36b5fc4fb313810d8556783824e34fb33587bcd
Wilhelmsen <wwilhelmsen@sogn.no>
2025-03-27 14:11:46 -0400

    Add equality-comparator for plottables
    I mean it's alright but I could improve it

--- visualizer/plottables.py
+++ visualizer/plottables.py
@@ -1,17 +1,41 @@
 #!/usr/bin/env python3
 import dataclasses
-
-# NOTE: Importing torch increases plottables.py individual test run-time by ~2.69 times
-# NOTE: I'm not dealing with that
+import numpy as np
+import torch
 
 
 @dataclasses.dataclass
 class Plottables:
-    model: object = None  # Actually type is nn.Moule
+    model: torch.nn.Module = None
     selected_layer: str = None
-    dataset_intermediary: list = None  # Type is really list[torch.Tensor]
-    dataset_plottable: list = None  # Type is really list[torch.Tensor]
+    dataset_intermediary: list[torch.tensor] = None
+    dataset_plottable: list[torch.tensor] = None
     image_plottable: tuple = None
+
+    def __eq__(self, other):
+        """
+        This is because torch and numpy are so cagey about whether two of
+        their arrays are equal. Like come on.
+        """
+        are_true = []
+
+        for a, b in zip(dataclasses.astuple(self), dataclasses.astuple(other)):
+
+            if type(a) is not type(b):
+                are_true.append(False)
+
+            else:
+                match type(a):
+                    case torch.Tensor:
+                        are_true.append(torch.equal(a, b))
+
+                    case np.ndarray:
+                        are_true.append(np.array_equal(a, b))
+
+                    case _:
+                        are_true.append(a == b)
+
+        return all(are_true)
 
     def __repr__(self):
         d = dataclasses.asdict(self)
